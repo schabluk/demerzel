0ed89752f40762935ba147b9aeac735a9dd03fb5
Lukasz Schabek <lukasz.schabek@gmail.com>
2019-01-20 12:33:46 +0100

    Configuration for testing JSX/TSX.

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   // roots: ['<rootDir>/src', '<rootDir>'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
     '^.+\\.jsx?$': 'babel-jest',
   },
   projects: ['<rootDir>/packages/*/jest.config.js'],
-  snapshotSerializers: [
-    // 'enzyme-to-json/serializer'
-  ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testPathIgnorePatterns: ['/node_modules/', '/\\./'],
