3c732038bdea1c8a58a7d3fbe2e86ab99924e62b
Lukasz Schabek <lukasz.schabek@gmail.com>
2019-01-20 12:43:44 +0100

    Update test examples.

--- packages/webapp/modules/Bar.test.tsx
+++ packages/webapp/modules/Bar.test.tsx
@@ -1,12 +1,18 @@
+/**
+ * This is an example of testing TypeScript React component.
+ */
+
 import * as React from 'react'
 import { render } from 'react-testing-library'
 
 import Bar from './Bar'
 
 describe('TypeScript with React Testing Library', () => {
-  it('Shows "Hello from Bar on level 1"', () => {
+  const expected = 'Hello from Bar on level 1'
+
+  it(`Shows "${expected}"`, () => {
     const { getByText } = render(<Bar title='Bar' level={1} />)
 
-    expect(getByText('Hello from Bar on level 1')).not.toBeNull()
+    expect(getByText(expected)).not.toBeNull()
   })
 })
--- packages/webapp/modules/Foo.test.jsx
+++ packages/webapp/modules/Foo.test.jsx
@@ -1,12 +1,18 @@
+/**
+ * This is an example of testing JavaScript React component.
+ */
+
 import React from 'react'
 import { render } from 'react-testing-library'
 
 import Foo from './Foo'
 
 describe('JavaScript with React Testing Library', () => {
-  it('Shows "Hello world!"', () => {
+  const expected = 'Hello World!'
+
+  it(`Shows "${expected}"`, () => {
     const { getByText } = render(<Foo />)
 
-    expect(getByText('Hello World!')).not.toBeNull()
+    expect(getByText(expected)).not.toBeNull()
   })
 })
